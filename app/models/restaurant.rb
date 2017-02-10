f9acc56b11de03f4972b9b3481fe8ff5406590cf
rikuto <rkt.w2024@outlook.jp>
2017-02-10 21:24:08 +0900

    url fixed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,9 +6,9 @@ class Restaurant < ApplicationRecord
 	presence: {message: "店名を入力してください。"}
 
 	validates :start_t,
-	presence: {message: "開店時間を正しく入力してください。"},format: { with: /\A[0-9]+\z/ , message: "半角数字で入力してください"},numericality: {greater_than_or_equal_to: 0,less_than: 25}
+	presence: {message: "開店時間を正しく入力してください。"},format: { with: /\A[0-9]+\z/ , message: "半角数字で入力してください"},numericality: {greater_than_or_equal_to: 0,less_than: 24}
 
 	validates :end_t,
-	presence: {message: "閉店時間を正しく入力してください。"},format: { with: /\A[0-9]+\z/ , message: "半角数字で入力してください"},numericality: {greater_than_or_equal_to: 0,less_than: 25}
+	presence: {message: "閉店時間を正しく入力してください。"},format: { with: /\A[0-9]+\z/ , message: "半角数字で入力してください"},numericality: {greater_than_or_equal_to: 1,less_than: 25}
 
 end
